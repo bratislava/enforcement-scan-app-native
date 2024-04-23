ee69b9b45adf45981f75829aea386a5a3b1fd1ae
PavelFilo <61841194+PavelFilo@users.noreply.github.com>
2024-04-23 16:55:40 +0200

    feat: other photo detail (#34)
    * feat: othre photo detail
    
    * translations
    
    * fix: photo detail search param into type

---
+++ app/(app)/offence/photos/detail.tsx
@@ -0,0 +1,102 @@
+import { Camera, CameraCapturedPicture, FlashMode } from 'expo-camera'
+import { router, useLocalSearchParams } from 'expo-router'
+import { useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Image } from 'react-native'
+
+import CameraBottomSheet from '@/components/camera/CameraBottomSheet'
+import FullScreenCamera from '@/components/camera/FullScreenCamera'
+import ScreenView from '@/components/screen-layout/ScreenView'
+import IconButton from '@/components/shared/IconButton'
+import { useCameraPermission } from '@/modules/permissions/useCameraPermission'
+import { useOffenceStoreContext } from '@/state/OffenceStore/useOffenceStoreContext'
+import { useSetOffenceState } from '@/state/OffenceStore/useSetOffenceState'
+
+type PhotoDetailSearchParams = {
+  index: string
+}
+
+const AppRoute = () => {
+  const { t } = useTranslation()
+  const { index } = useLocalSearchParams<PhotoDetailSearchParams>()
+  const photoIndex = Number(index)
+
+  const { setOffenceState } = useSetOffenceState()
+  const photos = useOffenceStoreContext((state) => state.photos)
+
+  const ref = useRef<Camera>(null)
+
+  const [photo, setPhoto] = useState<CameraCapturedPicture | null>(photos[photoIndex] || null)
+  const [isRetaking, setIsRetaking] = useState(false)
+  const [flashMode, setFlashMode] = useState<FlashMode>(FlashMode.off)
+  const [loading, setLoading] = useState(false)
+
+  useCameraPermission({ autoAsk: true })
+
+  const takePicture = async () => {
+    setLoading(true)
+    const capturedPhoto = await ref.current?.takePictureAsync()
+
+    if (!capturedPhoto) {
+      setLoading(false)
+
+      return
+    }
+
+    setPhoto(capturedPhoto)
+
+    setLoading(false)
+  }
+
+  const selectPicture = () => {
+    if (!(photo && isRetaking)) return
+
+    const newPhotos = [...photos]
+    newPhotos.splice(photoIndex, 1, photo)
+
+    setOffenceState({ photos: newPhotos })
+    router.back()
+  }
+
+  const retakePicture = () => {
+    setIsRetaking(true)
+    setPhoto(null)
+  }
+
+  return (
+    <ScreenView
+      hasBackButton
+      title={t('offence.picture.detail.title')}
+      options={{
+        headerRight: () => (
+          <IconButton
+            accessibilityLabel={t('offence.picture.detail.retake')}
+            name="cached"
+            onPress={retakePicture}
+          />
+        ),
+      }}
+      className="h-full"
+    >
+      {photo ? (
+        <Image source={{ uri: photo?.uri || photos[photoIndex].uri }} style={{ flex: 1 }} />
+      ) : (
+        <FullScreenCamera ref={ref} flashMode={flashMode} />
+      )}
+
+      {isRetaking ? (
+        <CameraBottomSheet
+          hasPhoto={!!photo}
+          flashMode={flashMode}
+          isLoading={loading}
+          takePicture={takePicture}
+          selectPicture={selectPicture}
+          retakePicture={retakePicture}
+          setFlashMode={setFlashMode}
+        />
+      ) : null}
+    </ScreenView>
+  )
+}
+
+export default AppRoute
