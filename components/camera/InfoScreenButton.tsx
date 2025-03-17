d98cc792f7c4b646117b3f603617641604611a00
PavelFilo <pvlfilo@gmail.com>
2025-03-17 15:37:54 +0100

    #150 change typo and restrict info button to zone only offences

--- components/camera/InfoScreenButton.tsx
+++ components/camera/InfoScreenButton.tsx
@@ -2,13 +2,17 @@ import { Link } from 'expo-router'
 import { useTranslation } from 'react-i18next'
 
 import IconButton from '@/components/shared/IconButton'
+import { getRoleByKey } from '@/modules/backend/constants/roles'
 import { useOffenceStoreContext } from '@/state/OffenceStore/useOffenceStoreContext'
 
 export const InfoScreenButton = () => {
   const { t } = useTranslation()
-  const generatedEcv = useOffenceStoreContext((state) => state.ecv)
+  const ecv = useOffenceStoreContext((state) => state.ecv)
+  const roleKey = useOffenceStoreContext((state) => state.roleKey)
 
-  return generatedEcv ? (
+  const role = getRoleByKey(roleKey)
+
+  return ecv && role?.actions.zone ? (
     <Link href="/scan/info" className="absolute left-0 top-0 z-10 m-2.5">
       <IconButton
         name="info"
