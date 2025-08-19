72fdc3fe565b842024bee9264fb3c901e92532da
PavelFilo <pvlfilo@gmail.com>
2025-08-19 15:26:58 +0200

    bump version to 1.3.2

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'Skenovacia aplikácia',
     slug: 'enforcement-scan-app',
     scheme: 'enforcement-scan-app',
-    version: '1.3.1',
+    version: '1.3.2',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
